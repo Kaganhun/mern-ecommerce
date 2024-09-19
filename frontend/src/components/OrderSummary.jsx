d494ca6eac60b70a35262981b128cf96617bcc97
Kaganhun <58555231+Kaganhun@users.noreply.github.com>
2024-09-19 13:47:09 +0300

    Update OrderSummary.jsx

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51Pzz3gP9UgnMFQHtBzg6wlSa38dOz04QsMmnp9ahc50hJthfHbYq2YU5LTQRie6IOAOTI7TyTqJoCbV6sGQ7Y3NM00HoBaoAp6"
 );
 
 const OrderSummary = () => {
